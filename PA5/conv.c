bd41349fb2875edefb3ed42c17f2a8acbdbf9a1e
Jiwon Kang <rkd8923@gmail.com>
2019-12-08 21:20:54 +0900

    remove useless multiply

--- PA5/conv.c
+++ PA5/conv.c
@@ -7,28 +7,25 @@ void convolution(const int M, const int N, const int *input, int *output, const 
 	{
 		for(int j = 0 ; j < N ; j++)
 		{
-			if (i - 1 < 0) output[i * N + j] += 0;
-			else {
-				if (j - 1 < 0) output[i * N + j] += 0;
-				else output[i * N + j] += input[(i-1) * N + (j - 1)] * filter[0][0];
-				output[i * N + j] += input[(i-1) * N + j] * filter[0][1];
-				if (j + 1 >= N) output[i * N + j] += 0;
-				else output[i * N + j] += input[(i-1) * N + (j + 1)] * filter[0][2];
+			int locate = i * N + j;
+			int first = (i - 1) * N;
+			int second = i * N;
+			int third = (i + 1) * N;
+
+			if (i - 1 >= 0) {
+				if (j - 1 >= 0) output[locate] += input[first + (j - 1)] * filter[0][0];
+				output[locate] += input[first + j] * filter[0][1];
+				if (j + 1 < N) output[locate] += input[first + (j + 1)] * filter[0][2];
 			}
 
-			if (j - 1 < 0) output[i * N + j] += 0;
-			else output[i * N + j] += input[i * N + (j - 1)] * filter[1][0];
-			output[i * N + j] += input[i * N + j] * filter[1][1];
-			if (j + 1 >= N) output[i * N + j] += 0;
-			else output[i * N + j] += input[i * N + (j + 1)] * filter[1][2];
+			if (j - 1 >= 0) output[locate] += input[second + (j - 1)] * filter[1][0];
+			output[locate] += input[locate] * filter[1][1];
+			if (j + 1 < N) output[locate] += input[second + (j + 1)] * filter[1][2];
 
-			if (i + 1 >= M) output[i * N + j] += 0;
-			else {
-				if (j - 1 < 0) output[i * N + j] += 0;
-				else output[i * N + j] += input[(i + 1) * N + (j - 1)] * filter[2][0];
-				output[i * N + j] += input[(i + 1) * N + j] * filter[2][1];
-				if (j + 1 >= N) output[i * N + j] += 0;
-				else output[i * N + j] += input[(i + 1) * N + (j + 1)] * filter[2][2];
+			if (i + 1 < M) {
+				if (j - 1 >= 0) output[locate] += input[third + (j - 1)] * filter[2][0];
+				output[locate] += input[third + j] * filter[2][1];
+				if (j + 1 < N) output[locate] += input[third + (j + 1)] * filter[2][2];
 			}
 		}
 	}
