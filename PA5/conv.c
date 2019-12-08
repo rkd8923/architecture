5ef5fc04af26748346586f5c1a4fa79fd118cedd
Jiwon Kang <rkd8923@gmail.com>
2019-12-09 00:17:41 +0900

    use register var

--- PA5/conv.c
+++ PA5/conv.c
@@ -1,58 +1,34 @@
 #include "conv.h"
-// static int BLOCK = 4;
-void convolution(const int M, const int N, const int *input, int *output, const int filter[3][3]) 
+void convolution(const register int M, const register int N, const register int *input, register int *output, const int filter[3][3]) 
 {
+	register int _filter[9] = {
+		filter[0][0], filter[0][1], filter[0][2],
+		filter[1][0], filter[1][1], filter[1][2],
+		filter[2][0], filter[2][1], filter[2][2]
+	};
 	//Write your code here
-	for(int i = 0 ; i < M ; i += 1)
+	for(register int i = 0 ; i < M ; i ++)
 	{
-		for(int j = 0 ; j < N ; j += 1)
+		for(register int j = 0 ; j < N ; j ++)
 		{
-			// for (int ii = i; ii < i + BLOCK; ii++) {
-			// 	for (int jj = j; jj < j + BLOCK; jj++) {
-			// 		int locate = ii * N + jj;
-			// 		int first = (ii - 1) * N;
-			// 		int locate = ii * N;
-			// 		int third = (ii + 1) * N;
-			// 		if (ii - 1 >= 0) {
-			// 			if (jj - 1 >= 0) output[locate] += input[first + (jj - 1)] * filter[0][0];
-			// 			output[locate] += input[first + jj] * filter[0][1];
-			// 			if (jj + 1 < N) output[locate] += input[first + (jj + 1)] * filter[0][2];
-			// 		}
-
-			// 		if (jj - 1 >= 0) output[locate] += input[locate + (jj - 1)] * filter[1][0];
-			// 		output[locate] += input[locate] * filter[1][1];
-			// 		if (jj + 1 < N) output[locate] += input[locate + (jj + 1)] * filter[1][2];
-
-			// 		if (ii + 1 < M) {
-			// 			if (jj - 1 >= 0) output[locate] += input[third + (jj - 1)] * filter[2][0];
-			// 			output[locate] += input[third + jj] * filter[2][1];
-			// 			if (jj + 1 < N) output[locate] += input[third + (jj + 1)] * filter[2][2];
-			// 		}
-			// 	}
-			// }
-
-			int locate = i * N + j;
-			
+			register int locate = i * N + j;
 			if (i - 1 >= 0) {
-				int first = locate - N;
-				if (j - 1 >= 0) output[locate] += input[first - 1] * filter[0][0];
-				output[locate] += input[first] * filter[0][1];
-				if (j + 1 < N) output[locate] += input[first + 1] * filter[0][2];
+				register int first = locate - N;
+				if (j - 1 >= 0) output[locate] += input[first - 1] * _filter[0];
+				output[locate] += input[first] * _filter[1];
+				if (j + 1 < N) output[locate] += input[first + 1] * _filter[2];
 			}
 
-			if (j - 1 >= 0) output[locate] += input[locate - 1] * filter[1][0];
-			output[locate] += input[locate] * filter[1][1];
-			if (j + 1 < N) output[locate] += input[locate + 1] * filter[1][2];
+			if (j - 1 >= 0) output[locate] += input[locate - 1] * _filter[3];
+			output[locate] += input[locate] * _filter[4];
+			if (j + 1 < N) output[locate] += input[locate + 1] * _filter[5];
 
 			if (i + 1 < M) {
-				int third = locate + N;
-				if (j - 1 >= 0) output[locate] += input[third - 1] * filter[2][0];
-				output[locate] += input[third] * filter[2][1];
-				if (j + 1 < N) output[locate] += input[third + 1] * filter[2][2];
+				register int third = locate + N;
+				if (j - 1 >= 0) output[locate] += input[third - 1] * _filter[6];
+				output[locate] += input[third] * _filter[7];
+				if (j + 1 < N) output[locate] += input[third + 1] * _filter[8];
 			}
 		}
 	}
 }
-
-
-
