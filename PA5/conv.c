3425bca986833729052ca3cbe1640b8d5161fd73
rkd8923 <rkd8923@gmail.com>
2019-12-09 14:44:38 +0900

    3

--- PA5/conv.c
+++ PA5/conv.c
@@ -13,27 +13,27 @@ void convolution(const register int M, const register int N, const register int 
 		for(register int j = 0; j < N ; j += 2)
 		{
 			register int locate = i * N + j;
-			if (i - 1 >= 0) {
+			if (i >= 1) {
 				register int first = locate - N;
-				if (j - 1 >= 0) output[locate] += input[first - 1] * _filter[0];
+				if (j >= 1) output[locate] += input[first - 1] * _filter[0];
 				output[locate] += input[first] * _filter[1];
 				if (j + 1 < N) output[locate] += input[first + 1] * _filter[2];
 			}
 
-			if (j - 1 >= 0) output[locate] += input[locate - 1] * _filter[3];
+			if (j >= 1) output[locate] += input[locate - 1] * _filter[3];
 			output[locate] += input[locate] * _filter[4];
 			if (j + 1 < N) output[locate] += input[locate + 1] * _filter[5];
 
 			if (i + 1 < M) {
 				register int third = locate + N;
-				if (j - 1 >= 0) output[locate] += input[third - 1] * _filter[6];
+				if (j >= 1) output[locate] += input[third - 1] * _filter[6];
 				output[locate] += input[third] * _filter[7];
 				if (j + 1 < N) output[locate] += input[third + 1] * _filter[8];
 			}
 
 			if (j == N - 1) break;
 			locate++;
-			if (i - 1 >= 0) {
+			if (i >= 1) {
 				register int first = locate - N;
 				if (j >= 0) output[locate] += input[first - 1] * _filter[0];
 				output[locate] += input[first] * _filter[1];
