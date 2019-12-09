a071caf3e3d898d1c0f0b9d52bc132c1bf9258c0
Jiwon Kang <rkd8923@gmail.com>
2019-12-09 13:54:16 +0900

    1

--- PA5/conv.c
+++ PA5/conv.c
@@ -9,7 +9,7 @@ void convolution(const register int M, const register int N, const register int 
 	//Write your code here
 	for(register int i = 0 ; i < M ; i ++)
 	{
-		for(register int j = 0; j < N ; j += 2)
+		for(register int j = 0; j < N ; j += 1)
 		{
 			register int locate = i * N + j;
 			if (i - 1 >= 0) {
@@ -30,25 +30,25 @@ void convolution(const register int M, const register int N, const register int 
 				if (j + 1 < N) output[locate] += input[third + 1] * _filter[8];
 			}
 
-			if (j + 1 == N) break;
-			locate++;
-			if (i - 1 >= 0) {
-				register int first = locate - N;
-				if (j >= 0) output[locate] += input[first - 1] * _filter[0];
-				output[locate] += input[first] * _filter[1];
-				if (j + 2 < N) output[locate] += input[first + 1] * _filter[2];
-			}
+			// if (j + 1 == N) break;
+			// locate++;
+			// if (i - 1 >= 0) {
+			// 	register int first = locate - N;
+			// 	if (j >= 0) output[locate] += input[first - 1] * _filter[0];
+			// 	output[locate] += input[first] * _filter[1];
+			// 	if (j + 2 < N) output[locate] += input[first + 1] * _filter[2];
+			// }
 
-			if (j >= 0) output[locate] += input[locate - 1] * _filter[3];
-			output[locate] += input[locate] * _filter[4];
-			if (j + 2 < N) output[locate] += input[locate + 1] * _filter[5];
+			// if (j >= 0) output[locate] += input[locate - 1] * _filter[3];
+			// output[locate] += input[locate] * _filter[4];
+			// if (j + 2 < N) output[locate] += input[locate + 1] * _filter[5];
 
-			if (i + 1 < M) {
-				register int third = locate + N;
-				if (j >= 0) output[locate] += input[third - 1] * _filter[6];
-				output[locate] += input[third] * _filter[7];
-				if (j + 2 < N) output[locate] += input[third + 1] * _filter[8];
-			}
+			// if (i + 1 < M) {
+			// 	register int third = locate + N;
+			// 	if (j >= 0) output[locate] += input[third - 1] * _filter[6];
+			// 	output[locate] += input[third] * _filter[7];
+			// 	if (j + 2 < N) output[locate] += input[third + 1] * _filter[8];
+			// }
 		}
 	}
 }
