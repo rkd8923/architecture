0c15c937174043099ea4624bd033700fd7256860
Jiwon Kang <rkd8923@gmail.com>
2019-12-09 13:59:39 +0900

    test

--- PA5/conv.c
+++ PA5/conv.c
@@ -10,7 +10,7 @@ void convolution(const register int M, const register int N, const register int 
 	for(register int i = 0 ; i < M ; i ++)
 	{
 		register int j = 0;
-		for(; j < N ; j += 2)
+		for(; j < N ; j += 4)
 		{
 			register int locate = i * N + j;
 			if (i - 1 >= 0) {
@@ -49,8 +49,44 @@ void convolution(const register int M, const register int N, const register int 
 				output[locate] += input[third] * _filter[7];
 				if (j + 2 < N) output[locate] += input[third + 1] * _filter[8];
 			}
+						locate++;
+			if (i - 1 >= 0) {
+				register int first = locate - N;
+				if (j >= 0) output[locate] += input[first - 1] * _filter[0];
+				output[locate] += input[first] * _filter[1];
+				if (j + 2 < N) output[locate] += input[first + 1] * _filter[2];
+			}
+
+			if (j >= 0) output[locate] += input[locate - 1] * _filter[3];
+			output[locate] += input[locate] * _filter[4];
+			if (j + 2 < N) output[locate] += input[locate + 1] * _filter[5];
+
+			if (i + 1 < M) {
+				register int third = locate + N;
+				if (j >= 0) output[locate] += input[third - 1] * _filter[6];
+				output[locate] += input[third] * _filter[7];
+				if (j + 2 < N) output[locate] += input[third + 1] * _filter[8];
+			}
+						locate++;
+			if (i - 1 >= 0) {
+				register int first = locate - N;
+				if (j >= 0) output[locate] += input[first - 1] * _filter[0];
+				output[locate] += input[first] * _filter[1];
+				if (j + 2 < N) output[locate] += input[first + 1] * _filter[2];
+			}
+
+			if (j >= 0) output[locate] += input[locate - 1] * _filter[3];
+			output[locate] += input[locate] * _filter[4];
+			if (j + 2 < N) output[locate] += input[locate + 1] * _filter[5];
+
+			if (i + 1 < M) {
+				register int third = locate + N;
+				if (j >= 0) output[locate] += input[third - 1] * _filter[6];
+				output[locate] += input[third] * _filter[7];
+				if (j + 2 < N) output[locate] += input[third + 1] * _filter[8];
+			}
 		}
-		
+
 
 	}
 }
